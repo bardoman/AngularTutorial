a9222ba3bc926460520b4b6884bb0e678484e8ee
beifler <beifler@us.ibm.com>
2023-02-23 16:34:11 -0700

    z

---
+++ mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice',power:'niceness' },
+  { id: 13, name: 'Bombasto' ,power:'bullshit'},
+  { id: 14, name: 'Celeritas' ,power:'speed'},
+  { id: 15, name: 'Magneta',power:'magnets' },
+  { id: 16, name: 'RubberMan',power:'stretchy' },
+  { id: 17, name: 'Dynama',power:'electric' },
+  { id: 18, name: 'Dr. IQ' ,power:'smart'},
+  { id: 19, name: 'Magma' ,power:'hot'},
+  { id: 20, name: 'Tornado' ,power:'spin'}
+];
